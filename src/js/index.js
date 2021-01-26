f9a99826efccebb385f61d9826398a35c56d0461
Anastasia Fabricieva <douughhnutt@gmail.com>
2021-01-26 20:25:26 +0300

    Декстоп верстка

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-import "./import/modules";
+import './import/modules';
+
+const mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  speed: 1300,
+  autoplay: {
+    delay: 3000,
+  },
+  flipEffect: {
+    slideShadows: false,
+  },
+})
